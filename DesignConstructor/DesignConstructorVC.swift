57faf49f466235fdeb10cbd1cdd8e40b7dbd3893
veselski-sergei <sveselski167@gmail.com>
2023-04-30 23:50:55 +0300

    Organize code

---
+++ DesignConstructor/DesignConstructorVC.swift
@@ -0,0 +1,16 @@
+//
+//  DesignConstructorVC.swift
+//  StreetLab
+//
+//  Created by Sergey Veselski on 30.04.2023.
+//
+
+import UIKit
+
+class DesignConstructorVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
